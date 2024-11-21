d96ba85477ac2ecac91a22b8a598410bec828245
Anjali Raut <Anjali.Raut@ELIGARF.TECH>
2024-11-21 08:44:23 +0530

    theme changes

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -18,9 +18,9 @@ function Header() {
         <img src={logo} alt="" className="w-20 h-20" />
         <div className="flex bg-slate-200 p-2 w-full items-center mx-5 rounded-full">
           <HiOutlineSearch />
-          <input type="text" className="bg-transparent outline-none mx-5 dark:bg-cyan-50" placeholder="Search Games" />
+          <input type="text" className="bg-transparent outline-none mx-5" placeholder="Search Games" />
         </div>
-        {theme=='light' ?<HiMoon className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer" onClick={()=>{setTheme('dark')}} /> :<HiSun className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer" onClick={()=>{setTheme('light')}}/> }
+        {theme=='light' ?<HiMoon className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer" onClick={()=>{setTheme('dark');localStorage.setItem('theme','dark')}} /> :<HiSun className="text-[35px] bg-slate-200 text-black p-1 rounded-full cursor-pointer" onClick={()=>{setTheme('light');localStorage.setItem('theme','light')}}/> }
         
         
       </div>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
 
 function HomePage() {
   return (
-    <div>HomePage</div>
-  )
+    <div className="grid grid-cols-4">
+      <div className="bg-red-300 h-full hidden md:block">Genre</div>
+      <div className="col-span-4 md:col-span-3 bg-blue-400">main</div>
+    </div>
+  );
 }
 
-export default HomePage
+export default HomePage;
